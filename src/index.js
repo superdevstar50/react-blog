46c74983d3c3622f1337e6749423b850d698ca6b
superdevstar50 <superdevstar50@gmail.com>
2017-08-30 03:26:26 +0530

    axios requests added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
+import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
