abf1facd206468a4d39d3cebc8f24f07df94f9bd
superdevstar50 <superdevstar50@gmail.com>
2017-08-30 23:17:29 +0530

    onsubmit added for forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -16,9 +16,15 @@ class PostsNew extends Component {
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const {handleSubmit} = this.props;
+
         return(
-            <form>
+            <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
                 <Field
                     name="title"
                     label="Title"
@@ -34,6 +40,7 @@ class PostsNew extends Component {
                     label="Post Content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
