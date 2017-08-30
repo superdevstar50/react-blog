fac89e6bb31d00cef2f2ef8f4899b8e4872242df
superdevstar50 <superdevstar50@gmail.com>
2017-08-31 00:54:40 +0530

    post created

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,7 @@ export function fetchPosts() {
     };
 }
 
-export function createPost(values) {
+export function createPost(values, callback) {
     const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
 
     return {
