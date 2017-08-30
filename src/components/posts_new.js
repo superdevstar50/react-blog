acf18142cda9d6a67de5873747e6c315d216fc1f
superdevstar50 <superdevstar50@gmail.com>
2017-08-30 19:09:36 +0530

    redux form inputs added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return(
+            <div>
+                <input
+                    { ...field.input }
+                />
+            </div>
+        );
+    }
+
     render() {
         return(
             <form>
                 <Field
                     name="title"
+                    component={this.renderTitleField}
                 />
             </form>
         );
