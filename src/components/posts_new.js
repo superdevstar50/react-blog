5cc72cd626b4372bb9f3bba409a0c33750b6bcf1
superdevstar50 <superdevstar50@gmail.com>
2017-08-31 00:06:53 +0530

    create post added and tested

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
     renderField(field) {
+        const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
         return(
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     type="text"
                     className="form-conrol"
                     { ...field.input }
                 />
-                {field.meta.touched ? field.meta.error : ''}
+                <div className="text-danger">{field.meta.touched ? field.meta.error : ''}</div>
             </div>
         );
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values);
     }
 
     render() {
         const {handleSubmit} = this.props;
-
+        const btn = {marginLeft: '5px'}
         return(
             <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
                 <Field
@@ -41,6 +45,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" style={btn} className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -67,4 +72,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
