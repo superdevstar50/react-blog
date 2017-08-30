c95b7531e98718eeffd519c4c82112a36ce47c61
superdevstar50 <superdevstar50@gmail.com>
2017-08-31 01:15:50 +0530

    posts show

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action) {
     switch(action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            return { ...state, [action.payload.data.id]: action.payload.data };
         default:
             return state;
     }
