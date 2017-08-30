e6343c542ba09bcaf858df3c97237520ff343e3e
superdevstar50 <superdevstar50@gmail.com>
2017-08-31 02:31:57 +0530

    delete post added

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions/index';
-import { FETCH_POST } from '../actions/index';
+import { FETCH_POST, DELETE_POST, FETCH_POSTS } from '../actions/index';
 
 export default function(state = {}, action) {
     switch(action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
         case FETCH_POST:
